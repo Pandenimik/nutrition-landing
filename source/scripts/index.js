89e5665b6231c268701827db754a9c416afd766f
Pandenimik <89628864+Pandenimik@users.noreply.github.com>
2025-01-10 11:50:45 +0300

    Добавляет слайдер в конец без модалки.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,28 +1,8 @@
 /* в этот файл добавляет скрипты*/
+import {ItcSlider} from '../vendor/slider/itc-slider.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-  const scrollbar = `${document.body.clientWidth - window.innerWidth }px`;
-  let elSlider1;
-
-  document.querySelector('[data-target="modal-1"]').addEventListener('click', (e) => {
-    e.preventDefault();
-    document.body.style.overflow = 'hidden';
-    document.querySelector('#modal-1').style.marginLeft = scrollbar;
-    document.querySelector('#modal-1').classList.add('modal-open');
-    if (elSlider1 === undefined) {
-      elSlider1 = ItcSlider.getOrCreateInstance('.itc-slider-1', { loop: false });
-    } else {
-      elSlider1.reset();
-    }
-  });
-
-  document.addEventListener('click', (e) => {
-    if (e.target.closest('[data-target="close"]')) {
-      e.preventDefault();
-      const modal = e.target.closest('.modal');
-      document.body.style.overflow = 'visible';
-      modal.style.marginLeft = '0px';
-      modal.classList.remove('modal-open');
-      elSlider1.autoplay.stop();
-    }
+  ItcSlider.getOrCreateInstance('.itc-slider', {
+    loop: false, // без зацикливания
   });
 });
